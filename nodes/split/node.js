efaaec6ab65c3891df8c107b9d767321d2d3d4ba
Rob Halff <rob.halff@gmail.com>
2014-02-13 12:04:40 +0100

    fix

--- nodes/split/node.js
+++ nodes/split/node.js
@@ -6,14 +6,15 @@ output = function(cb) {
 
       var m = input.regexp.match(/^\/(.*)\/([gmi]*)/);
       if(m.length === 3) {
-        cb({ out: input.in.split(new RegExp(m[1]), m[2]) });
+        var re = new RegExp(m[1], m[2]);
+        cb({ out: input.in.split(re) });
       } else {
         cb({ error: ['RegExp not recognized:', input.regexp, 'required format: /..../gm' ].join(' ') });
       }
     } else if(input.seperator) {
       cb({ out: input.in.split(input.seperator) });
     } else {
-      cb({ out: [] });
+      cb({ error: 'no input seperator or regexp' });
     }
 
     done();
