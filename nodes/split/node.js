8c556cc43f8a330f9d9dc570d8cddd65ebf0e1c0
Rob Halff <rob.halff@gmail.com>
2014-02-13 11:58:40 +0100

    update split

--- nodes/split/node.js
+++ nodes/split/node.js
@@ -3,7 +3,13 @@ output = function(cb) {
   try {
 
     if(input.regexp) {
-      cb({ out: input.in.split(new RegExp(input.regexp)) });
+
+      var m = input.regexp.match(/^\/(.*)\/([gmi]*)/);
+      if(m.length === 3) {
+        cb({ out: input.in.split(new RegExp(m[1]), m[2]) });
+      } else {
+        cb({ error: ['RegExp not recognized:', input.regexp, 'required format: /..../gm' ].join(' ') });
+      }
     } else if(input.seperator) {
       cb({ out: input.in.split(input.seperator) });
     } else {
