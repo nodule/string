a3b3c3bb05840ef4ba95a6bb1892f70076e70035
Rob Halff <rob.halff@gmail.com>
2014-02-13 11:44:36 +0100

    update regexp

--- nodes/split/node.js
+++ nodes/split/node.js
@@ -1,1 +1,20 @@
-output.out = input. in .split(input.seperator)
+output = function(cb) {
+
+  try {
+
+    if(input.regexp) {
+      cb({ out: input.in.split(new RegExp(input.regexp)) });
+    } else if(input.seperator) {
+      cb({ out: input.in.split(input.seperator) });
+    } else {
+      cb({ out: [] });
+    }
+
+    done();
+
+  } catch(err) {
+    cb({ error: err });
+    done();
+  }
+
+};
